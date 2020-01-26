6215b6a3fa4d3b93c7843c5282f54eb6c42a2198
KT Sutter <hermioninnyorjustaninny@gmail.com>
2020-01-25 18:11:15 -0800

    updated to be within .1 rmse of color transfer

--- color_transfer.py
+++ color_transfer.py
@@ -2,17 +2,14 @@ import cv2
 import numpy as np
 import sys
 
-def convert_color_space_RGB_to_LMS(img_RGB, lab):
+def convert_color_space_RGB_to_LMS(img_RGB):
     rgb_to_lms = [[0.3811, 0.5783, 0.0402], [0.1967, 0.7244, 0.0782], [0.0241, 0.1288, 0.8444]]
     img_LMS = np.zeros_like(img_RGB, dtype=np.float32)
     height = img_RGB.shape[0]
     width = img_RGB.shape[1]
     for i in range(0, height):
         for j in range(0, width):
-            img_LMS[i, j] = np.matmul(rgb_to_lms, img_RGB[i, j])
-            # Have to take log of LMS if this is for the Lab conversion
-            if lab:
-                img_LMS[i,j] = np.log10(img_LMS[i,j])
+            img_LMS[i,j] = np.matmul(rgb_to_lms, img_RGB[i,j])
     return img_LMS
 
 def convert_color_space_LMS_to_RGB(img_LMS):
@@ -29,13 +26,11 @@ def convert_color_space_LMS_to_RGB(img_LMS):
 
 def convert_color_space_BGR_to_RGB(img_BGR):
     #copy BGR in reverse
-    img_RGB = img_BGR[..., ::-1].copy()
-    return img_RGB
+    return img_BGR[..., ::-1].astype(float).copy()
 
 def convert_color_space_RGB_to_BGR(img_RGB):
     #copy RGB in reverse
-    img_BGR = img_RGB[..., ::-1].copy()
-    return img_BGR
+    return img_RGB[..., ::-1].astype(float).copy()
 
 def convert_color_space_RGB_to_Lab(img_RGB):
     '''
@@ -43,18 +38,18 @@ def convert_color_space_RGB_to_Lab(img_RGB):
     '''
     lms_to_lab1 = [[1, 1, 1], [1, 1, -2], [1, -1, 0]]
     lms_to_lab2 = [[1/np.sqrt(3), 0, 0], [0, 1/np.sqrt(6), 0], [0, 0, 1/np.sqrt(2)]]
+    lms_to_lab = np.matmul(lms_to_lab2, lms_to_lab1)
     height = img_RGB.shape[0]
     width = img_RGB.shape[1]
 
     # pass in true so that log10 of result is calculated -- necessary for this conversion
-    img_LMS = convert_color_space_RGB_to_LMS(img_RGB, True)
+    img_LMS = convert_color_space_RGB_to_LMS(img_RGB)
 
     img_Lab = np.zeros_like(img_RGB,dtype=np.float32)
     for i in range(0, height):
         for j in range(0, width):
-            img_Lab[i,j] = np.matmul(lms_to_lab1, img_LMS[i,j])
-            img_Lab[i,j] = np.matmul(lms_to_lab2, img_Lab[i,j])
-
+            img_LMS[i,j] = np.log10(img_LMS[i,j])
+            img_Lab[i,j] = np.matmul(lms_to_lab, img_LMS[i,j])
     return img_Lab
 
 def convert_color_space_Lab_to_RGB(img_Lab):
@@ -71,7 +66,6 @@ def convert_color_space_Lab_to_RGB(img_Lab):
             img_LMS[i,j] = np.matmul(lab_to_lms1, img_Lab[i,j])
             img_LMS[i,j] = np.matmul(lab_to_lms2, img_LMS[i,j])
             img_LMS[i,j] = np.power(10, img_LMS[i,j])
-
     img_RGB = convert_color_space_LMS_to_RGB(img_LMS)
     return img_RGB
 
@@ -79,10 +73,9 @@ def convert_color_space_RGB_to_CIECAM97s(img_RGB):
     '''
     convert image color space RGB to CIECAM97s
     '''
-    lms_to_ciecam97s = [[2.00, 1.00, 0.05],[1.00, -1.09, 0.09],[0.11, 0.11, -0.22]]
+    lms_to_ciecam97s = [[2.0000, 1.0000, 0.0500],[1.0000, -1.0900, 0.0900],[0.1100, 0.1100, -0.2200]]
     img_CIECAM97s = np.zeros_like(img_RGB,dtype=np.float32)
-    # Pass in false so that log10 won't be taken -- unnecessary for this conversion, but important for RGB -> Lab
-    img_LMS = convert_color_space_RGB_to_LMS(img_RGB, False)
+    img_LMS = convert_color_space_RGB_to_LMS(img_RGB)
     height = img_RGB.shape[0]
     width = img_RGB.shape[1]
     for i in range(0, height):
@@ -94,20 +87,17 @@ def convert_color_space_CIECAM97s_to_RGB(img_CIECAM97s):
     '''
     convert image color space CIECAM97s to RGB
     '''
-    cie_to_lms = [[0.32787, 0.32159, 0.20608], [0.32787, -0.63534, -0.18540], [0.32787, -0.15688, -4.53512]]
+    cie_to_lms = np.linalg.inv([[2.0000, 1.0000, 0.0500], [1.0000, -1.0900, 0.0900], [0.1100, 0.1100, -0.2200]])
     img_LMS = np.zeros_like(img_CIECAM97s,dtype=np.float32)
     height = img_CIECAM97s.shape[0]
     width = img_CIECAM97s.shape[1]
     for i in range(0, height):
         for j in range(0, width):
-            img_LMS[i, j] = np.matmul(cie_to_lms, img_CIECAM97s[i, j])
+            img_LMS[i,j] = np.matmul(cie_to_lms, img_CIECAM97s[i,j])
     img_RGB = convert_color_space_LMS_to_RGB(img_LMS)
     return img_RGB
 
 def transfer_color(img_src, img_target):
-
-    img_src.astype(float)
-    img_target.astype(float)
 
     # get mean for each color channel of src
     red_mean1 = np.mean(img_src[:,:,0])
@@ -136,11 +126,11 @@ def transfer_color(img_src, img_target):
     std_blue = blue_std2/blue_std1
 
     # subtract mean from each color channel of src
-    img_result = img_src - np.array([red_mean1, green_mean1, blue_mean1])
+    img_result = img_src - np.array([red_mean1, green_mean1, blue_mean1], dtype=np.float32)
     # multiply each color channel by standard deviation
-    img_result *= np.array([std_red, std_green, std_blue])
+    img_result *= np.array([std_red, std_green, std_blue], dtype=np.float32)
     # add mean of each channel of target to each channel of src
-    img_result += np.array([red_mean2, green_mean2, blue_mean2])
+    img_result += np.array([red_mean2, green_mean2, blue_mean2], dtype=np.float32)
 
     return img_result
 
@@ -199,7 +189,14 @@ def color_transfer(img_RGB_source, img_RGB_target, option):
         img_RGB_new = color_transfer_in_Lab(img_RGB_source, img_RGB_target)
     elif option == 'in_CIECAM97s':
         img_RGB_new = color_transfer_in_CIECAM97s(img_RGB_source, img_RGB_target)
+    img_RGB_new = np.clip(img_RGB_new, 0, 1)
     return img_RGB_new
+
+
+#def rmse(apath,bpath):
+#    a = cv2.imread(apath)
+#    b = cv2.imread(bpath)
+#    print(np.sqrt(np.mean((a-b)**2)))
 
 if __name__ == "__main__":
     print('==================================================')
@@ -211,21 +208,43 @@ if __name__ == "__main__":
     path_file_image_result_in_Lab = sys.argv[3]
     path_file_image_result_in_RGB = sys.argv[4]
     path_file_image_result_in_CIECAM97s = sys.argv[5]
+#    path_file_result = sys.argv[6]
 
     # ===== read input images
     # img_RGB_source: is the image you want to change the its color
     # img_RGB_target: is the image containing the color distribution that you want to change the img_RGB_source to (transfer color of the img_RGB_target to the img_RGB_source)
-    img_RGB_source = cv2.imread(path_file_image_source, cv2.IMREAD_COLOR)
-    img_RGB_target = cv2.imread(path_file_image_target,cv2.IMREAD_COLOR)
+    img_RGB_source = np.float32(cv2.imread(path_file_image_source, cv2.IMREAD_COLOR))
+    img_RGB_source[:,:,0] /= 255.0	
+    img_RGB_source[:,:,1] /= 255.0
+    img_RGB_source[:,:,2] /= 255.0
+    img_RGB_target = np.float32(cv2.imread(path_file_image_target,cv2.IMREAD_COLOR))
+    img_RGB_target[:,:,0] /= 255.0	
+    img_RGB_target[:,:,1] /= 255.0
+    img_RGB_target[:,:,2] /= 255.0
 
     img_RGB_new_Lab = color_transfer(img_RGB_source, img_RGB_target, option='in_Lab')
+    img_RGB_new_Lab[:,:,0] *= 255.0
+    img_RGB_new_Lab[:,:,1] *= 255.0
+    img_RGB_new_Lab[:,:,2] *= 255.0
+    img_RGB_new_Lab = np.floor(img_RGB_new_Lab)
     # save image to path_file_image_result_in_Lab
     cv2.imwrite(path_file_image_result_in_Lab, img_RGB_new_Lab)
+    # rmse(path_file_image_result_in_Lab, path_file_result)
 
     img_RGB_new_RGB = color_transfer(img_RGB_source, img_RGB_target, option='in_RGB')
+    img_RGB_new_RGB[:,:,0] *= 255.0
+    img_RGB_new_RGB[:,:,1] *= 255.0
+    img_RGB_new_RGB[:,:,2] *= 255.0
+    img_RGB_new_RGB = np.floor(img_RGB_new_RGB)
     # save image to path_file_image_result_in_Lab
     cv2.imwrite(path_file_image_result_in_RGB, img_RGB_new_RGB)
+    # rmse(path_file_image_result_in_RGB, path_file_result)
 
     img_RGB_new_CIECAM97s = color_transfer(img_RGB_source, img_RGB_target, option='in_CIECAM97s')
+    img_RGB_new_CIECAM97s[:,:,0] *= 255.0
+    img_RGB_new_CIECAM97s[:,:,1] *= 255.0
+    img_RGB_new_CIECAM97s[:,:,2] *= 255.0
+    img_RGB_new_CIECAM97s = np.floor(img_RGB_new_CIECAM97s)
     # save image to path_file_image_result_in_Lab
-    cv2.imwrite(path_file_image_result_in_CIECAM97s, img_RGB_new_CIECAM97s)
+    cv2.imwrite(path_file_image_result_in_CIECAM97s, img_RGB_new_CIECAM97s) 
+    # rmse(path_file_image_result_in_CIECAM97s, path_file_result)
